5ef8a7b70e4202e344db46e8e428a5f0077ccd80
TaishiMiyabe <75838736+TaishiMiyabe@users.noreply.github.com>
2021-02-03 23:05:21 +0900

    Initial commit

---
+++ Assets/ItemGenerator.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour
+{
+    public GameObject carPrefab;
+    //GameObject car = Instantiate(carPrefab);
+
+    public GameObject coinPrefab;
+    //GameObject coin = Instantiate(coinPrefab);
+
+    public GameObject conePrefab;
+    //GameObject cone = Instantiate(conePrefab);
+
+    //public GameObject UnityChan;
+
+    //課題
+    private Renderer targetRenderer;
+
+    //スタート地点
+    private int startPos = 15;
+    //ゴール地点
+    private int goalPos = 360;
+    //アイテムを出すx方向の範囲
+    private float posRange = 3.4f;
+    //アイテム自動生成されるz方向の範囲
+    private int zRange = 50;
+    //Unityちゃんのz軸方向の位置＋zRangeの値。このラインをUnityちゃんが超えたら、再び自動生成メソッドがかかる。
+    private float z = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //課題
+        this.targetRenderer = GetComponent<Renderer>();
+
+        Vector3 UnityPos = GameObject.Find("unitychan").transform.position;
+
+        z += UnityPos.z + zRange;
+        for(int i = startPos; i < z; i += 15)//Unitychan.transform.position.z+50
+        {
+            this.ItemGenerate(i);
+        }
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //課題
+        if (!targetRenderer.isVisible)
+        {
+            Destroy(this.gameObject);
+            Debug.Log("aaa");
+        }
+        Vector3 UnityPos = GameObject.Find("unitychan").transform.position;
+
+        
+        if (UnityPos.z >= startPos)//z % 15 == 0　はダメだった。15m進むごとに一つオブジェクト生成されるイメージだった。update()が1フレームごとに呼ばれる中、そのフレーム毎のUnityPos.zの値が全然整数じゃないから…？
+        {
+            z += zRange;
+            startPos += zRange;
+            for (int i = startPos; i < z; i += 15)//Unityちゃんが進むごとにアイテム生成されたけど、なんか変な作り方してる気がする。
+            {
+                this.ItemGenerate(i);
+            }
+        }
+    }
+
+    void ItemGenerate(float i)
+    {
+        int num = Random.Range(1, 11);
+        if (num <= 2)
+        {
+            for (float j = -1; j <= 1; j += 0.4f)
+            {
+                GameObject cone = Instantiate(conePrefab);
+                cone.transform.position = new Vector3(4 * j, cone.transform.position.y, i);
+            }
+        }
+        else
+        {
+            //レーンごとにアイテムを生成
+            for (int j = -1; j <= 1; j++)
+            {
+                //アイテムの種類を決める
+                int item = Random.Range(1, 11);
+
+                int offsetZ = Random.Range(-5, 6);
+
+                //60%coin 30%car 10%nothing
+                if (1 <= item && item <= 6)
+                {
+                    GameObject coin = Instantiate(coinPrefab);
+                    coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, i + offsetZ);
+
+                }
+                else if (7 <= item && item <= 9)
+                {
+                    GameObject car = Instantiate(carPrefab);
+                    car.transform.position = new Vector3(posRange * j, car.transform.position.y, i + offsetZ);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnityChanController : MonoBehaviour
+{
+    private Animator myAnimator;
+
+    private Rigidbody myRigidbody;
+
+    //前方向の速度
+    private float velocityZ = 16f;
+
+    //横方向の速度
+    private float velocityX = 10f;
+
+    //縦方向の速度(ジャンプ)
+    private float velocityY = 10f;
+
+    //左右の移動範囲
+    private float movableRange = 3.4f;
+
+    //動きを減速させる係数
+    private float coefficient = 0.99f;
+
+    private float TextCoefficient = 0.01f;
+
+    //ゲーム終了判定
+    private bool isEnd = false;
+
+    //ゲーム終了時に表示するテキスト
+    private GameObject stateText;
+
+    //スコア表示用のテキスト
+    private GameObject scoreText;
+    private int score = 0;//得点
+
+    //テキストの透明度
+    private float a_color;
+
+    //ボタン
+    private bool isLButtonDown = false;
+    private bool isRButtonDown = false;
+    private bool isJButtonDown = false;
+
+    //private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.speed = 1;
+
+        this.myAnimator = GetComponent<Animator>();
+
+        //this.myAnimator.SetFloat("Speed", speed);
+
+        this.myAnimator.SetFloat("Speed", 1);
+
+        this.myRigidbody = GetComponent<Rigidbody>();
+
+        //スコアテキストオブジェクト取得
+        this.scoreText = GameObject.Find("ScoreText");
+
+        //ゲームスタート時テキスト
+        this.a_color = 1;
+
+        this.stateText = GameObject.Find("GameResultText");
+
+        this.stateText.GetComponent<Text>().text = "GAME START";
+
+        this.stateText.GetComponent<Text>().color = new Color(1, 1, 1, a_color);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isEnd)
+        {
+            //this.speed *= this.coefficient;
+            //this.myAnimator.SetFloat("Speed", speed); ←ユニティちゃんの動きは止まるけど、物体本体がずっとスライドするように動き続ける。座標の移動も止めないといけない。
+
+            this.velocityX *= this.coefficient;
+            this.velocityY *= this.coefficient;
+            this.velocityZ *= this.coefficient;
+            this.myAnimator.speed *= this.coefficient;
+
+            //テキストを徐々に表示...テキストが出てくるタイミングが遅い。。。
+            this.a_color += this.TextCoefficient;
+            this.stateText.GetComponent<Text>().color = new Color(1, 1, 1, a_color);
+        }
+        else
+        {
+           //スタート直後、"GAME START"の文字をだんだん薄くする。
+                this.a_color -= this.TextCoefficient;
+                this.stateText.GetComponent<Text>().color = new Color(1, 1, 1, a_color);
+            
+        }
+
+
+        float inputVelocityX = 0;
+        float inputVelocityY = 0;
+
+        //ジャンプしてないときにスペースキーを押す
+        if((Input.GetKey(KeyCode.Space)||this.isJButtonDown)&&this.transform.position.y < 0.5f)
+        {
+            this.myAnimator.SetBool("Jump", true);
+
+            inputVelocityY = this.velocityY;
+        }
+        else
+        {
+            inputVelocityY = this.myRigidbody.velocity.y;
+        }
+
+        //Jumpステートの場合にはJumpにfalseをセットする
+        if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            this.myAnimator.SetBool("Jump", false);
+        }
+
+        if((Input.GetKey(KeyCode.LeftArrow)||this.isLButtonDown)&& -this.movableRange < this.transform.position.x)
+        {
+            inputVelocityX = -this.velocityX;
+        }
+        if((Input.GetKey(KeyCode.RightArrow)||isRButtonDown)&& this.transform.position.x < this.movableRange)
+        {
+            inputVelocityX = this.velocityX;
+        }
+
+        this.myRigidbody.velocity = new Vector3(inputVelocityX, inputVelocityY, this.velocityZ);
+    }
+
+
+
+    //Triggerモードでほかのオブジェクトと衝突したときの処理
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag")
+        {
+            this.isEnd = true;
+            this.stateText.GetComponent<Text>().text = "GAME OVER";
+           
+        }
+
+        if(other.gameObject.tag == "GoalTag")
+        {
+            this.isEnd = true;
+            this.stateText.GetComponent<Text>().text = "CLEAR!!";
+        }
+
+        if(other.gameObject.tag == "CoinTag")
+        {
+            //接触したコインオブジェクトの破棄
+            Destroy(other.gameObject);
+
+            GetComponent<ParticleSystem>().Play();
+
+            this.score += 10;
+            this.scoreText.GetComponent<Text>().text = $"Score {score}pt ";
+        }
+    }
+
+
+    public void GetMyJumpButtonDown()
+    {
+        this.isJButtonDown = true;
+    }
+
+    public void GetMyJumpButtonUp()
+    {
+        this.isJButtonDown = false;
+    }
+
+    public void GetMyLeftButtonDown()
+    {
+        this.isLButtonDown = true;
+    }
+
+    public void GetMyLeftButtonUp()
+    {
+        this.isLButtonDown = false;
+    }
+
+    public void GetMyRightButtonDown()
+    {
+        this.isRButtonDown = true;
+    }
+
+    public void GetMyRightButtonUp()
+    {
+        this.isRButtonDown = false;
+    }
+}
